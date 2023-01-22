aeb494cdf329dd700983e91516062367e7e75bf5
AkuraDiary <asthiseta@gmail.com>
2023-01-22 16:14:11 +0700

    step 2 tutorial added landmark model

---
+++ hello world/data/model/Landmark.swift
@@ -0,0 +1,15 @@
+//
+//  Landmark.swift
+//  hello world
+//
+//  Created by asthiseta on 22/01/23.
+//
+
+import Foundation
+struct Landmark : Hashable, Codable{
+    var id : Int
+    var name : String
+    var park :String
+    var state :String
+    var description : String
+}
