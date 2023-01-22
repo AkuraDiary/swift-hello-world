348c7b6e239cf9bf66de1db205847e8bb01600c8
AkuraDiary <asthiseta@gmail.com>
2023-01-22 10:21:35 +0700

    tutorial 1 done

--- hello world/ContentView.swift
+++ hello world/ContentView.swift
@@ -10,20 +10,35 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         
-        Form{
-            Section{
-                Text("hello world")
-                Text("hello world")
-            }
-            Section{
-                Text("hello world")
-                Text("hello world")
-            }
+        VStack {
+            MapView().frame(height: 300)
+            CircleImage()
+                .offset(y:-130)
+                .padding(.bottom, -130)
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                HStack {
+                    Text("Joshua Tree National Park")
+                    Spacer()
+                    Text("California")
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                
+                Divider()
+                Text("Joshua Tree National Park")
+                    .font(.title2)
+                Text("Description text here")
+                
+            }.padding()
             
+            Spacer()
         }
-        
     }
+    
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
